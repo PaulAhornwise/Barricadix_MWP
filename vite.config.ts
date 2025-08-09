8d7d44548987d034e7b43fc49a2d5cbfeb2b5c73
Paul Eichwald <pauleichwald@gmx.de>
2025-08-09 18:53:18 +0200

    deploy: update docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,22 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, '.', '');
+  const isProd = mode === 'production';
+  return {
+    base: isProd ? '/Barricadix_MWP/' : '/',
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true,
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
